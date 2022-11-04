76a68731a87ced2213fcf016454639458d39e650
Diego Alonso Ojeda Medina <daom89@gmail.com>
2022-11-04 17:18:09 -0500

    Pagina inicial viajes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/*.{html,js}","./src/**/*.{html,js}"],
+  content: ["*.{html,js}","./public/*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
         'midnight': '#121063',
         'gray-dark': '#273444',
+        'principal': '#CC2D4A',
+        'secondary': '#8FA206',
+        'tertiary': '#61AEC9',
       },
       spacing: {
         '8xl': '96rem',
@@ -13,9 +16,39 @@ module.exports = {
       },
       borderRadius: {
         '4xl': '2rem',
-      }
+      },
+      backgroundImage: {
+        'sanFrancisco': "url('../img/sanFrancisco.jpg')",
+        'sanFranciscoDesktop': "url('../img/sanFranciscoDesktop.jpg')",
+        'yosemite': "url('../img/yosemite.jpg')",
+        'LA': "url('../img/LA.jpg')",
+        'seattle': "url('../img/seattle.jpg')",
+        'new_york': "url('../img/new_york.jpg')",
+        'norway': "url('../img/norway.jpg')",
+        'sydney': "url('../img/sydney.jpg')",
+        'miami': "url('../img/miami.jpg')",
+        'switzerland': "url('../img/switzerland.jpg')",
+        'bali': "url('../img/bali.jpg')",
+        'norway': "url('../img/norway.jpg')",
+        'chicago': "url('../img/chicago.jpg')",
+        'europe': "url('../img/europe.jpg')",
+        'iceland': "url('../img/iceland.jpg')",
+
+      },
+      textColor: {
+        'principal': '#CC2D4A',
+        'secondary': '#8FA206',
+        'tertiary': '#61AEC9',
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+      },
     },
 
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }

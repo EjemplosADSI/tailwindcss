24f51a34002c4976e66c42d35ee89739bba1eaa7
Diego Ojeda <daom89@gmail.com>
2022-11-04 09:31:22 -0500

    Basic config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./public/*.{html,js}","./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'midnight': '#121063',
+        'gray-dark': '#273444',
+      },
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    },
+
   },
   plugins: [],
 }
